81a2e408e30cce07988f633d21adcd0c08d1421b
IgorSantanaM <igorsantanamedeiros17@gmail.com>
2025-04-29 18:07:17 -0400

    (feat): Added Real World Sending Email

--- Rockaway.WebApp/Services/Mail/SmtpMailSender.cs
+++ Rockaway.WebApp/Services/Mail/SmtpMailSender.cs
@@ -1,11 +1,12 @@
-﻿using MailKit.Net.Smtp;
+using MailKit.Net.Smtp;
+using MailKit.Security;
 using MimeKit;
 
 namespace Rockaway.WebApp.Services.Mail {
 	public class SmtpMailSender(SmtpSettings settings) : IMailSender {
 		public async Task<string> SendAsync(MimeMessage message) {
 			using var smtp = new SmtpClient();
-			await smtp.ConnectAsync(settings.Host, settings.Port);
+			await smtp.ConnectAsync(settings.Host, settings.Port, SecureSocketOptions.StartTls);
 			if(settings.Authenticate) await smtp.AuthenticateAsync(settings.Username, settings.Password);
 			var result = await smtp.SendAsync(message);
 			await smtp.DisconnectAsync(true);
